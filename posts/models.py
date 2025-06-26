c06d34c28f0d7f2fc4a20498f0ddcf49cd2de83a
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-06-27 00:39:18 +0200

    Post Model Change

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 
 
 class Post(models.Model):
@@ -29,8 +28,9 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    image = CloudinaryField('image')  
-    
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_ltnkan', blank=True
+    )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
     )
