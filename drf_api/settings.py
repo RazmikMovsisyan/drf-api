e9849fede48782c68120ab5669a93df3142a6ba1
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-06-27 11:21:55 +0200

    Comment Out Database

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -40,17 +40,17 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
-if 'DEV' in os.environ:
-     DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': BASE_DIR / 'db.sqlite3',
-         }
-     }
-else:
-     DATABASES = {
-         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-     }
+# if 'DEV' in os.environ:
+#      DATABASES = {
+#          'default': {
+#              'ENGINE': 'django.db.backends.sqlite3',
+#              'NAME': BASE_DIR / 'db.sqlite3',
+#          }
+#      }
+# else:
+#      DATABASES = {
+#          'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#      }
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
