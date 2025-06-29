6e520565015c4181d364d09889cc08d2c4c68bd1
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-06-29 23:26:15 +0200

    Image sourced from Cloudinary

--- posts/serializers.py
+++ posts/serializers.py
@@ -11,6 +11,7 @@ class PostSerializer(serializers.ModelSerializer):
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
+    image = serializers.ReadOnlyField(source='image.url')
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
