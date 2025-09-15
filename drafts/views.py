e887516cca1a86339334eb9d13ce8bf926cb6292
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-09-15 17:41:14 +0200

    Draft: Publish as Post Fix

--- drafts/views.py
+++ drafts/views.py
@@ -1,8 +1,42 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from .models import Draft
-from .serializers import DraftSerializer, DraftDetailSerializer
+from .serializers import DraftSerializer
 from .permissions import IsAuthor
+from django.utils import timezone
+from .serializers import DraftSerializer, DraftDetailSerializer
+from .models import Draft, Post
+from django.utils import timezone
+
+class DraftPublish(generics.UpdateAPIView):
+    permission_classes = [IsAuthor]
+    serializer_class = DraftSerializer
+    queryset = Draft.objects.all()
+
+    def patch(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+
+            # 1. Post erstellen
+            post = Post.objects.create(
+                owner=instance.author,
+                title=getattr(instance, "title", ""),
+                content=instance.content,
+                image=instance.image,
+                created_at=timezone.now(),
+            )
+
+            instance.status = "published"
+            instance.published_at = timezone.now()
+            instance.save()
+
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
 
 
 class DraftList(generics.ListCreateAPIView):
@@ -20,19 +54,3 @@ class DraftDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthor]
     serializer_class = DraftDetailSerializer
     queryset = Draft.objects.all()
-
-
-class DraftPublish(generics.UpdateAPIView):
-    permission_classes = [IsAuthor]
-    serializer_class = DraftSerializer
-    queryset = Draft.objects.all()
-
-    def patch(self, request, *args, **kwargs):
-        try:
-            instance = self.get_object()
-            instance.publish()
-            return Response(status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response(
-                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
