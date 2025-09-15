74ef8fbf60cd338f97b6a466a274546d216f9b1c
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-09-16 00:25:20 +0200

    Remove: Unfortunate complete removal of drafts modell due limited time of PP5 submission and debugging of publishing draft as a post. What was functional: Create a draft. Edit a draft. Remove a draft. Publishing a draft as a post was functioning too. Making changes caused a bug and the time did not allow me to fix it before submission

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     "comments",
     "likes",
     "followers",
-    "drafts",
     "newsletter",
 ]
 
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -34,6 +34,5 @@ urlpatterns = [
     path("", include("comments.urls")),
     path("", include("likes.urls")),
     path("", include("followers.urls")),
-    path("", include("drafts.urls")),
     path("newsletter/", include("newsletter.urls")),
 ]
